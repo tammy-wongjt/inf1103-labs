b8f09dae956eeeff1f25b012ca6e324b7a2eb8b9
Tammy <2605401@sit.singaporetech.edu.sg>
2026-09-04 15:50:14 +0800

    Added Activity 4 and commented out activity 3

--- hello.py
+++ hello.py
@@ -13,14 +13,25 @@
 # print("Bio: ", bio)
 # print("followers: ", followers)
 
-# Activity 3
-followers = 100
+# # Activity 3
+# followers = 100
 
-followers += 50
-print("Day 1: ", followers)
+# followers += 50
+# print("Day 1: ", followers)
 
-followers += 20
-print("Day 2: ", followers)
+# followers += 20
+# print("Day 2: ", followers)
 
-followers -= 10
-print("Day 3: ", followers)
+# followers -= 10
+# print("Day 3: ", followers)
+
+# Activity 4: Interactive profile creator
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category:")
+
+print("\nInstagram Profile")
+print("==============================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
